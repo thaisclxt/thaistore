de7b46b2dff3b2a855efabda0bd1bb3ea9cd74d0
thaisclxt <thais.calixto@mail.uft.edu.br>
2023-02-02 14:19:37 -0300

    Extract header components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,60 +1,17 @@
 import { NextPage } from "next";
+import { GlobalStyle } from "../public/globalStyle";
 import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from 'react-redux';
-import styled from "styled-components";
-import { apiURL } from "../api";
-import { GlobalStyle } from "../public/globalStyle";
-import Product from "../components/product";
 import { increment, selectValue } from "../slices/counterSlice";
-
-
-interface Props {
-  title: string;
-  subtitle: string;
-}
-
-const Title = styled.div`
-  	@import url("https://fonts.googleapis.com/css2?family=Montserrat&display=swap");
-
-	color: #FFFFFF;
-	font-family: 'Montserrat';
-	font-style: normal;
-	mix-blend-mode: normal;
-	
-	& h1 {
-		font-weight: 600;
-		font-size: 40px;
-	}
-
-	& h2 {
-		font-weight: 300;
-		font-size: 20px;
-	}
-`
-
-const Wrapper = styled.section`
-	background: #0F52BA;
-	height: 101px;
-`;
-
-const Header: NextPage<Props> = ({ title, subtitle }) => {
-	return (
-		<Wrapper>
-			<Title>
-				<h1>{title}</h1>
-			</Title>
-			<Title>
-				<h2>{subtitle}</h2>
-			</Title>
-		</Wrapper>
-	);
-}
+import { apiURL } from "../api";
+import Product from "../components/product";
+import Header from "../components/header";
 
 const HomePage: NextPage = () => {
-	const page: Number = 1
-	const rows: Number = 5
-	const sortBy: String = "name"
-	const orderBy: String = "DESC"
+	const page: number = 1
+	const rows: number = 5
+	const sortBy: string = "name"
+	const orderBy: string = "DESC"
 
 	const [data, setData] = useState<Array<any>>([])
 
@@ -72,8 +29,7 @@ const HomePage: NextPage = () => {
 	return (
 		<div>
 			<GlobalStyle />
-
-			<Header title="MKS" subtitle="Sistemas"/>
+			<Header title="MKS" subtitle="Sistemas" count={count}/>
 
 			<div>
 				{data.map((item) => (
